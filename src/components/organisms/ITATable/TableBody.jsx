555e93c88b044c87cafc817730380476d77b3f81
Pabl0Parra <pabsblue16@gmail.com>
2023-01-27 20:10:47 +0100

    delete old sort function

--- src/components/organisms/ITATable/TableBody.jsx
+++ src/components/organisms/ITATable/TableBody.jsx
@@ -1,32 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-const-assign */
-import React, { useContext, useCallback } from 'react'
+import React, { useContext } from 'react'
 import { TableContext } from './store/context'
 import { TableCell } from './styles'
 
 function TableBody() {
   const { state } = useContext(TableContext)
-  const { data, columns, sortBy, sortDirection } = state
-
-  // Sort the data based on the current sort column and direction
-  const sortedData = useCallback(() => {
-    if (!sortBy) {
-      return data
-    }
-    return [...data].sort((a, b) => {
-      if (a[sortBy] < b[sortBy]) {
-        return sortDirection === 'asc' ? -1 : 1
-      }
-      if (a[sortBy] > b[sortBy]) {
-        return sortDirection === 'asc' ? 1 : -1
-      }
-      return 0
-    })
-  }, [data, sortBy, sortDirection])
+  const { data, columns } = state
 
   return (
     <tbody>
-      {sortedData().map((d) => (
+      {data.map((d) => (
         <tr key={d.id}>
           {columns.map((col) => (
             <TableCell key={`${d.id}-${col.id}`}>
